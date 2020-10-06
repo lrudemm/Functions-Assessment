26002178315d1e585044419c9e359ba91ad7f300
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-10-06 16:47:45 -0400

    strated on the main.js file

---
+++ main.js
@@ -0,0 +1,9 @@
+/* Define a function named firstLetterName. 
+    It should take a string name as a parameter 
+    and alert the message 
+    "The name <NAME> starts with the letter <FIRSTLETTEROFNAME> */
+
+function firstLetterName() {
+    
+}
+
