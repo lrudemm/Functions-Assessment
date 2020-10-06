8005971fd2f996c1bb321792ab683ce543de4a12
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-10-06 16:56:35 -0400

    updated all notes for assignment in the main.js file

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 /* Define a function named firstLetterName. 
     It should take a string name as a parameter 
     and alert the message 
-    "The name <NAME> starts with the letter <FIRSTLETTEROFNAME> */
+    "The name <NAME> starts with the letter <FIRSTLETTEROFNAME>
+    HINT: to get a single character of a string, 
+    you can use bracket syntax like you used to access array elements */
 
 function firstLetterName() {
-    
+
 }
 
+/* Define a function divisibleByTwo. It should take a single parameter number
+    and determine whether the number input is an even or odd number. 
+    If it's even, the function should return true. 
+    If it's odd, the function should return false.
+        HINT: You'll need to use the % operator. */
+
+
+
+/* Define a function named largestNum. It should take an array arr as a parameter
+    and loop through that array. 
+    The function should return the largest number in the array. */
+
+
+
+/* After defining all the functions, 
+    call each function at the bottom of the 
+    JavaScript file with the appropriate arguments. */
