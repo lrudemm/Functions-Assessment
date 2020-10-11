00aa2dbf795cf6cb4834cb3f53ca316fa6a69bc5
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-10-11 18:25:09 -0400

    final project to submit with group additions

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ function firstLetterName(NAME) {
     alert ("The name " + NAME + " starts with the letter " +  FIRSTLETTEROFNAME);
 }
 
-firstLetterName("Larry");
 
 /* Define a function divisibleByTwo. It should take a single parameter number
     and determine whether the number input is an even or odd number. 
@@ -18,14 +17,36 @@ firstLetterName("Larry");
     If it's odd, the function should return false.
     HINT: You'll need to use the % operator. */
 
+function divisibleByTwo(number){
+     if(number%2 == 0)
+        return true;
+        return false;
+    }
 
 
 /* Define a function named largestNum. It should take an array arr as a parameter
     and loop through that array. 
     The function should return the largest number in the array. */
+function largestNum(arr){
+    var largest = 0;
+        for(var i=0; i < arr.length; i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+            return largest;
+    }
 
 
 
 /* After defining all the functions, 
     call each function at the bottom of the 
     JavaScript file with the appropriate arguments. */
+    var name = "Larry";
+    var number = 13;
+    var arr = [1, 3, 2, 6, 4, 70];
+
+    firstLetterName(name);
+
+    console.log(divisibleByTwo(number));
+    console.log(largestNum(arr));
